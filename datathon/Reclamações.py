64979050f6e217fd70dd3b63662ee2c6a8e12e78
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2025-03-31 14:36:44 +0100

    o

--- datathon/Reclamações.py
+++ datathon/Reclamações.py
@@ -26,13 +26,13 @@ def analyze_sentiment(text):
     return sum(sentiments) / len(sentiments) if sentiments else 0  # Média dos sentimentos
 
 # Carregar o dataset
-df_clientes = pd.read_excel("analise.xlsx", sheet_name=1)
+df_clientes = pd.read_csv("clientes.csv")
 
 # Aplicar a análise de sentimento em paralelo
 with ThreadPoolExecutor() as executor:
-    df_clientes['sentiment'] = list(executor.map(analyze_sentiment, df_clientes['Detalhes_Reclamações']))
+    df_clientes['sentimento'] = list(executor.map(analyze_sentiment, df_clientes['Detalhes_Reclamações']))
 
 # Salvar os resultados em um novo arquivo Excel
-df_clientes.to_excel('sentiment_analysis_results.xlsx', index=False)
+df_clientes.to_csv('clientes.csv')
 
 print("Análise de sentimento concluída e salva em 'sentiment_analysis_results.xlsx'.")
